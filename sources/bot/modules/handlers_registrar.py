1b0debfc37b7f4998ac4c3502a3f7becc8f9a245
NikitaZotov <nikita.zotov.belarus@gmail.com>
2021-11-14 14:56:39 +0300

    refactor(handler): change name to handlers registrar, rename lists

--- sources/bot/modules/handlers_registrar.py
+++ sources/bot/modules/handlers_registrar.py
@@ -2,14 +2,14 @@ from bot.loggers import LogInstaller
 from bot.state_machine import StateMachine
 
 
-class Handler:
+class HandlersRegistrar:
     _logger = LogInstaller.get_default_logger(__name__, LogInstaller.INFO)
-    callbacks = []
-    _handlers = {}
+    _callback_contexts = []
+    _handler_types = {}
 
     def __init__(self, machine: StateMachine):
         self._machine = machine
-        self._handlers.update(
+        self._handler_types.update(
             {
                 "message_handler": self._machine.register_message_handler,
                 "callback_query_handler": self._machine.callback_query_handler,
@@ -19,7 +19,7 @@ class Handler:
     @staticmethod
     def message_handler(*custom_filters, commands=None, regexp=None, content_types=None, state=None, **kwargs):
         def decorator(callback):
-            callback_params = {
+            callback_context = {
                 "handler": "message_handler",
                 "callback": callback,
                 "custom_filters": custom_filters,
@@ -28,9 +28,9 @@ class Handler:
                 "content_types": content_types,
                 "state": state,
             }
-            callback_params.update(kwargs)
+            callback_context.update(kwargs)
 
-            Handler.callbacks.append(callback_params)
+            HandlersRegistrar._callback_contexts.append(callback_context)
 
             return callback
 
@@ -39,23 +39,23 @@ class Handler:
     @staticmethod
     def callback_query_handler(*custom_filters, state=None, **kwargs):
         def decorator(callback):
-            callback_params = {
+            callback_context = {
                 "handler": "callback_query_handler",
                 "callback": callback,
                 "custom_filters": custom_filters,
                 "state": state,
             }
-            callback_params.update(kwargs)
+            callback_context.update(kwargs)
 
-            Handler.callbacks.append(callback_params)
+            HandlersRegistrar._callback_contexts.append(callback_context)
 
             return callback
 
         return decorator
 
-    def register(self):
-        for callback_params in Handler.callbacks:
-            register_message_handler = self._handlers.get(callback_params["handler"])
+    def process(self):
+        for callback_params in HandlersRegistrar._callback_contexts:
+            register_message_handler = self._handler_types.get(callback_params["handler"])
             del callback_params["handler"]
 
             custom_filters = callback_params["custom_filters"]
