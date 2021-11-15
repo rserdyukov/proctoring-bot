c197ab4a1d83e705e7360805bca862e916b6c98d
MksmOrlov <tawnhall8.5@gmail.com>
2021-11-15 17:27:08 +0300

    feat(spreadsheet): implement universal handler

--- sources/bot/modules/spreadsheet/spreadsheet_handler.py
+++ sources/bot/modules/spreadsheet/spreadsheet_handler.py
@@ -1,15 +1,19 @@
-"""
-"""
 import httplib2
 import apiclient
 from oauth2client.service_account import ServiceAccountCredentials
-from typing import List
+from typing import List, Dict
+
+from bot.loggers import LogInstaller
 
 
 class SpreadsheetHandler:
-    def __init__(self, spreadsheet_id, file_name: str):
+    _logger = LogInstaller.get_default_logger(__name__, LogInstaller.DEBUG)
+
+    def __init__(self, spreadsheet_id, file_name: str, sheet_attributes_dict: Dict[str, List[str]]):
         self._spreadsheet_id = spreadsheet_id
         self._credentials_file = file_name
+        self._sheet_attributes_dict = sheet_attributes_dict
+        self._created_sheet_list = list()
 
         self._credentials = ServiceAccountCredentials.from_json_keyfile_name(
             self._credentials_file,
@@ -18,7 +22,16 @@ class SpreadsheetHandler:
         self._http_auth = self._credentials.authorize(httplib2.Http())
         self._service = apiclient.discovery.build("sheets", "v4", http=self._http_auth)
 
-    def __create_student_sheet(self, row_count: int, column_count: int) -> None:
+    def __pop_sheet_title(self):
+        titles = list(self._sheet_attributes_dict.keys())
+        sheet_title = titles[len(self._sheet_attributes_dict.keys()) - len(self._created_sheet_list) - 1]
+        attributes = self._sheet_attributes_dict.get(sheet_title)
+        self._created_sheet_list.append(sheet_title)
+        return sheet_title, attributes
+
+    def __create_sheet(self, row_count: int, column_count: int) -> None:
+        sheet_title, attributes = self.__pop_sheet_title()
+
         self._service.spreadsheets().batchUpdate(
             spreadsheetId=self._spreadsheet_id,
             body={
@@ -26,7 +39,7 @@ class SpreadsheetHandler:
                     {
                         "addSheet": {
                             "properties": {
-                                "title": "Студенты",
+                                "title": sheet_title,
                                 "gridProperties": {"rowCount": row_count, "columnCount": column_count},
                             }
                         }
@@ -41,26 +54,28 @@ class SpreadsheetHandler:
                 "valueInputOption": "USER_ENTERED",
                 "data": [
                     {
-                        "range": "Студенты!A1:D1",
+                        "range": f"{sheet_title}!A1:D1",
                         "majorDimension": "ROWS",
-                        "values": [["username", "ФИО", "Группа", "Подруппа"]],
+                        "values": [attributes],
                     }
                 ],
             },
         ).execute()
 
-    def create_spreadsheet(self, row_count: int, column_count: int) -> None:
+    def create_spreadsheet(self, spreadsheet_title: str, row_count: int, column_count: int) -> None:
+        sheet_title, attributes = self.__pop_sheet_title()
+
         spreadsheet = (
             self._service.spreadsheets()
             .create(
                 body={
-                    "properties": {"title": "Информация о людях", "locale": "ru_RU"},
+                    "properties": {"title": spreadsheet_title, "locale": "ru_RU"},
                     "sheets": [
                         {
                             "properties": {
                                 "sheetType": "GRID",
                                 "sheetId": 0,
-                                "title": "Преподаватели",
+                                "title": sheet_title,
                                 "gridProperties": {"rowCount": row_count, "columnCount": column_count},
                             }
                         }
@@ -71,95 +86,109 @@ class SpreadsheetHandler:
         )
 
         self._spreadsheet_id = spreadsheet["spreadsheetId"]
+        self._logger.debug(
+            f"Open spreadsheet in https://docs.google.com/spreadsheets/d/"
+            f"{self._spreadsheet_id}/edit#gid=0"
+        )
 
         self._service.spreadsheets().values().batchUpdate(
             spreadsheetId=self._spreadsheet_id,
             body={
                 "valueInputOption": "USER_ENTERED",
-                "data": [{"range": "Преподаватели!A1:B1", "majorDimension": "ROWS", "values": [["username", "ФИО"]]}],
+                "data": [{"range": f"{sheet_title}!A1:D1", "majorDimension": "ROWS", "values": [attributes]}],
             },
         ).execute()
 
-        self.__create_student_sheet(row_count, column_count)
+        while len(self._created_sheet_list) != len(self._sheet_attributes_dict.keys()):
+            self.__create_sheet(row_count, column_count)
 
         self.__get_permissions()
 
     def __get_permissions(self) -> None:
         drive_service = apiclient.discovery.build("drive", "v3", http=self._http_auth)
-
         drive_service.permissions().create(
             fileId=self._spreadsheet_id, body={"type": "anyone", "role": "reader"}, fields="id"
         ).execute()
 
-    def __get_spreadsheet_range(self, corner_from: str, corner_to: str):
+    def __get_sheet_range(self, spreadsheet_title: str, corner_from: str, corner_to: str):
         return (
             self._service.spreadsheets()
             .values()
             .batchGet(
                 spreadsheetId=self._spreadsheet_id,
-                ranges=[f"Студенты!{corner_from}:{corner_to}"],
+                ranges=[f"{spreadsheet_title}!{corner_from}:{corner_to}"],
                 valueRenderOption="FORMATTED_VALUE",
                 dateTimeRenderOption="FORMATTED_STRING",
             )
             .execute()
         )
 
-    def __get_username_spreadsheet_range(self):
-        return self.__get_spreadsheet_range("A2", "A1000")
+    def __get_first_column_sheet_range(self, spreadsheet_title: str):
+        return self.__get_sheet_range(spreadsheet_title, "A1", "A1000")
 
-    def __update_spreadsheet_row(self, row_number: int, values: List[str]):
+    def __update_spreadsheet_row(self, spreadsheet_title: str, row_number: int, values: List[str]):
         self._service.spreadsheets().values().batchUpdate(
             spreadsheetId=self._spreadsheet_id,
             body={
                 "valueInputOption": "USER_ENTERED",
                 "data": [
-                    {"range": f"Студенты!A{row_number}:D{row_number}", "majorDimension": "ROWS", "values": [values]}
+                    {
+                        "range": f"{spreadsheet_title}!A{row_number}:D{row_number}",
+                        "majorDimension": "ROWS",
+                        "values": [values],
+                    },
                 ],
             },
         ).execute()
 
-    def add_student(self, username: str, name: str, group: str, subgroup: str) -> None:
-        results = self.__get_username_spreadsheet_range()
+    def add_row(self, spreadsheet_title: str, row: List[str]) -> None:
+        first_row_element = row[0]
+        results = self.__get_first_column_sheet_range(spreadsheet_title)
         sheet_values = results["valueRanges"][0]["values"]
-
         row_number = len(sheet_values) + 1
 
-        for user in sheet_values:
-            if not user:
+        for sheet_rows in sheet_values:
+            if not sheet_rows:
                 row_number = sheet_values.index([]) + 1
                 break
-            if user[0] == username:
-                row_number = sheet_values.index([username]) + 1
+            if sheet_rows[0] == first_row_element:
+                row_number = sheet_values.index([first_row_element]) + 1
                 break
 
-        self.__update_spreadsheet_row(row_number, [username, name, group, subgroup])
+        self.__update_spreadsheet_row(spreadsheet_title, row_number, row)
 
-    def remove_student(self, username: str) -> None:
-        results = self.__get_username_spreadsheet_range()
+    def remove_row(self, spreadsheet_title: str, first_row_element: str) -> None:
+        results = self.__get_first_column_sheet_range(spreadsheet_title)
         sheet_values = results["valueRanges"][0]["values"]
+        row_number = sheet_values.index([first_row_element]) + 1
 
-        row_number = sheet_values.index([username]) + 2
+        empty_string_list = []
+        for i in range(len(self._sheet_attributes_dict.get(spreadsheet_title))):
+            empty_string_list.append("")
 
-        self.__update_spreadsheet_row(row_number, ["", "", "", ""])
+        self.__update_spreadsheet_row(spreadsheet_title, row_number, empty_string_list)
 
-    def get_student_usernames(self) -> list:
-        results = self.__get_username_spreadsheet_range()
+    def get_first_column_sheet_range(self, spreadsheet_title: str) -> list:
+        results = self.__get_first_column_sheet_range(spreadsheet_title)
         sheet_values = results["valueRanges"][0]["values"]
         return sheet_values
 
-    def get_student_by_username(self, username: str) -> dict:
-        results = self.__get_spreadsheet_range("A2", "D1000")
+    def get_row_by_first_element(self, spreadsheet_title: str, element: str) -> dict:
+        alphabet_start_index = 64
+        right_corner = chr(alphabet_start_index + len(self._sheet_attributes_dict.get(spreadsheet_title)))
+        results = self.__get_sheet_range(spreadsheet_title, "A2", f"{right_corner}1000")
 
         if results["valueRanges"][0].get("values"):
             sheet_values = results["valueRanges"][0]["values"]
         else:
             return {}
 
-        user = {}
-        for user_row in sheet_values:
-            for user_name in user_row:
-                if user_name == username:
-                    user["ФИО"] = user_row[1]
-                    user["Группа"] = user_row[2]
-                    user["Подгруппа"] = user_row[3]
-        return user
+        row = {}
+        i = 0
+        for sheet_row in sheet_values:
+            if sheet_row and sheet_row[0] == element:
+                for attribute in self._sheet_attributes_dict.get(spreadsheet_title):
+                    row[attribute] = sheet_row[i]
+                    i += 1
+
+        return row
