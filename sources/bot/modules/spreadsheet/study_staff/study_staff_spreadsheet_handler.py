ac131d6ff4cc4258c03beeceb1f5d9f64bb8db41
MksmOrlov <tawnhall8.5@gmail.com>
2021-11-15 22:33:46 +0300

    fix(spreadsheet): add username parametr in get methods

--- sources/bot/modules/spreadsheet/study_staff/study_staff_spreadsheet_handler.py
+++ sources/bot/modules/spreadsheet/study_staff/study_staff_spreadsheet_handler.py
@@ -13,8 +13,8 @@ class StudyStaffSpreadsheetHandler:
     def create_spreadsheet(self, spreadsheet_title="Информация о людях", row_count=1000, column_count=10) -> None:
         self._handler.create_spreadsheet(spreadsheet_title, row_count, column_count)
 
-    def add_student(self, name: str, group: str, subgroup: str) -> None:
-        self._handler.add_row(self._student_sheet_title, [name, group, subgroup])
+    def add_student(self, username: str, name: str, group: str, subgroup: str) -> None:
+        self._handler.add_row(self._student_sheet_title, [username, name, group, subgroup])
 
     def remove_student(self, username: str) -> None:
         self._handler.remove_row(self._student_sheet_title, username)
@@ -22,11 +22,11 @@ class StudyStaffSpreadsheetHandler:
     def get_student_usernames(self) -> List[str]:
         return self._handler.get_first_column_sheet_range(self._student_sheet_title)
 
-    def get_student_by_username(self, username: str) -> Dict[str:str]:
+    def get_student_by_username(self, username: str):
         return self._handler.get_row_by_first_element(self._student_sheet_title, username)
 
-    def add_teacher(self, name: str) -> None:
-        self._handler.add_row(self._teacher_sheet_title, [name])
+    def add_teacher(self, username: str, name: str) -> None:
+        self._handler.add_row(self._teacher_sheet_title, [username, name])
 
     def remove_teacher(self, username: str) -> None:
         self._handler.remove_row(self._teacher_sheet_title, username)
@@ -34,5 +34,5 @@ class StudyStaffSpreadsheetHandler:
     def get_teacher_usernames(self) -> List[str]:
         return self._handler.get_first_column_sheet_range(self._teacher_sheet_title)
 
-    def get_teacher_by_username(self, username: str) -> Dict[str:str]:
+    def get_teacher_by_username(self, username: str):
         return self._handler.get_row_by_first_element(self._teacher_sheet_title, username)
