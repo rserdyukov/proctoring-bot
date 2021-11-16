b249f7364f821988e77e6f7ec3e157e81de9cbb5
MksmOrlov <tawnhall8.5@gmail.com>
2021-11-16 16:32:13 +0300

    feat(spreadsheet): implement non-positional arguments and exceptions

--- sources/bot/modules/spreadsheet/study_staff/study_staff_spreadsheet_handler.py
+++ sources/bot/modules/spreadsheet/study_staff/study_staff_spreadsheet_handler.py
@@ -1,5 +1,6 @@
-from typing import List, Dict
+from typing import List
 
+from bot.exceptions import InvalidSpreadsheetAttributeException
 from bot.modules.spreadsheet.spreadsheet_handler import SpreadsheetHandler
 
 
@@ -16,8 +17,17 @@ class StudyStaffSpreadsheetHandler:
     def create_spreadsheet(self, spreadsheet_title="Информация о людях", row_count=1000, column_count=10) -> None:
         self._handler.create_spreadsheet(spreadsheet_title, row_count, column_count)
 
-    def add_student(self, username: str, name: str, group: str, subgroup: str) -> None:
-        self._handler.add_row(self._student_sheet_title, [username, name, group, subgroup])
+    def add_student(self, username: str, *args) -> None:
+        name, group, subgroup = args
+
+        if not name:
+            raise InvalidSpreadsheetAttributeException("Invalid name value")
+        elif not group:
+            raise InvalidSpreadsheetAttributeException("Invalid group value")
+        elif not subgroup:
+            raise InvalidSpreadsheetAttributeException("Invalid subgroup value")
+        else:
+            self._handler.add_row(self._student_sheet_title, [username, name, group, subgroup])
 
     def remove_student(self, username: str) -> None:
         self._handler.remove_row(self._student_sheet_title, username)
@@ -28,8 +38,13 @@ class StudyStaffSpreadsheetHandler:
     def get_student_by_username(self, username: str):
         return self._handler.get_row_by_first_element(self._student_sheet_title, username)
 
-    def add_teacher(self, username: str, name: str) -> None:
-        self._handler.add_row(self._teacher_sheet_title, [username, name])
+    def add_teacher(self, username: str, *args) -> None:
+        name = args[0]
+
+        if not name:
+            raise InvalidSpreadsheetAttributeException("Invalid name value")
+        else:
+            self._handler.add_row(self._teacher_sheet_title, [username, name])
 
     def remove_teacher(self, username: str) -> None:
         self._handler.remove_row(self._teacher_sheet_title, username)
