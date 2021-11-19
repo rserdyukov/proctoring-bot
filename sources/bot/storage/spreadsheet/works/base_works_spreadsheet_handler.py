a058d76d97742a4e0b3fe44e7830462c1f4b2f85
NikitaZotov <nikita.zotov.belarus@gmail.com>
2021-11-20 01:57:50 +0300

    feat(works_spreadsheet): implement works spreadsheet handler interface

---
+++ sources/bot/storage/spreadsheet/works/base_works_spreadsheet_handler.py
@@ -0,0 +1,8 @@
+from abc import ABCMeta
+
+from bot.storage.spreadsheet.base_spreadsheet_handler import BaseSpreadsheetHandler
+
+
+class BaseWorksSpreadsheetHandler(BaseSpreadsheetHandler):
+    __metaclass__ = ABCMeta
+    # todo: Formalise interface
---
+++ sources/bot/storage/spreadsheet/works/works_spreadsheet_handler.py
@@ -0,0 +1,13 @@
+from bot.storage.spreadsheet.works.base_works_spreadsheet_handler import BaseWorksSpreadsheetHandler
+
+
+class WorksSpreadsheetHandler(BaseWorksSpreadsheetHandler):
+    # todo: Implement works handler
+    def __init__(self, spreadsheet_id: str, token: str):
+        pass
+
+    def create_spreadsheet(self, spreadsheet_title=None, row_count=None, column_count=None) -> None:
+        pass
+
+    def accept_storage(self, storage):
+        storage.visit_works_handler(self)
