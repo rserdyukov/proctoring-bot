e8faefbb27d2ed4f54a225fc843e9b57a21fc6ca
NikitaZotov <nikita.zotov.belarus@gmail.com>
2021-11-20 01:59:11 +0300

    feat(base_handler): implement interface for visitor function

--- sources/bot/storage/spreadsheet/base_spreadsheet_handler.py
+++ sources/bot/storage/spreadsheet/base_spreadsheet_handler.py
@@ -1,12 +1,13 @@
 from abc import ABCMeta, abstractmethod
 
-from bot.exceptions import SpreadsheetHandlerException
-from bot.storage.base_spreadsheet_storage import BaseSpreadsheetStorage
-
 
 class BaseSpreadsheetHandler:
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def accept_storage(self, storage: BaseSpreadsheetStorage):
-        raise SpreadsheetHandlerException("Not implemented method")
+    def create_spreadsheet(self, spreadsheet_title=None, row_count=None, column_count=None) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def accept_storage(self, storage):
+        raise NotImplementedError
