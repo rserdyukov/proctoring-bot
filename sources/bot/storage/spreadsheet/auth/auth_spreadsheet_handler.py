70dccb9afba1e408b7080c7314762ec9e5e51494
NikitaZotov <nikita.zotov.belarus@gmail.com>
2021-11-20 01:50:00 +0300

    feat(spreadsheet): change args to kwargs and to not implemented error

--- sources/bot/storage/spreadsheet/auth/auth_spreadsheet_handler.py
+++ sources/bot/storage/spreadsheet/auth/auth_spreadsheet_handler.py
@@ -19,8 +19,10 @@ class AuthSpreadsheetHandler(BaseAuthSpreadsheetHandler):
     def create_spreadsheet(self, spreadsheet_title="Информация о людях", row_count=1000, column_count=10) -> None:
         self._handler.create_spreadsheet(spreadsheet_title, row_count, column_count)
 
-    def add_student(self, username: str, *args) -> None:
-        name, group, subgroup = args
+    def add_student(self, username: str, **kwargs) -> None:
+        name = kwargs.get("name")
+        group = kwargs.get("group")
+        subgroup = kwargs.get("subgroup")
 
         if not name:
             raise InvalidSpreadsheetAttributeException("Invalid name value")
@@ -40,8 +42,8 @@ class AuthSpreadsheetHandler(BaseAuthSpreadsheetHandler):
     def get_student_by_username(self, username: str) -> dict:
         return self._handler.get_row_by_first_element(self._student_sheet_title, username)
 
-    def add_teacher(self, username: str, *args) -> None:
-        name = args[0]
+    def add_teacher(self, username: str, **kwargs) -> None:
+        name = kwargs.get("name")
 
         if not name:
             raise InvalidSpreadsheetAttributeException("Invalid name value")
