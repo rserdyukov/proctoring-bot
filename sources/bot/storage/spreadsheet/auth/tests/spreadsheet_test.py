6fe33ebf504d3eb605f356d366358ca80dc5d6b2
MksmOrlov <tawnhall8.5@gmail.com>
2021-11-27 18:55:44 +0300

    fix(test): auth add methods use kwargs

--- sources/bot/storage/spreadsheet/auth/tests/spreadsheet_test.py
+++ sources/bot/storage/spreadsheet/auth/tests/spreadsheet_test.py
@@ -5,6 +5,6 @@ from bot.storage.spreadsheet.auth.auth_spreadsheet_handler import AuthSpreadshee
 
 class TestSpreadsheet(unittest.TestCase):
     def setUp(self):
-        token_path = "test_token.json"
+        token_path = "../../../../../tokens/auth_token.json"
         self.handler = AuthSpreadsheetHandler("", token_path)
         self.handler.create_spreadsheet()
