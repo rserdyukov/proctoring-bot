e63fbb7322e532d5b9b183f22dc040d8e7a94b02
NikitaZotov <nikita.zotov.belarus@gmail.com>
2021-12-17 00:32:34 +0300

    docs(proctoring_bot): add docs strings about implementation

--- sources/bot/proctoring_bot.py
+++ sources/bot/proctoring_bot.py
@@ -1,11 +1,18 @@
-from bot.bot import Bot
-from bot.loggers import LogInstaller
-from bot.modules.factory.handlers_factory import HandlersFactory
-from bot.state_machine import StateMachine
-from bot.storage.base_spreadsheet_storage import BaseSpreadsheetStorage
+"""
+Proctoring bot implementation module.
+"""
+from ..bot.bot import Bot
+from ..bot.loggers import LogInstaller
+from ..bot.modules.factory.handlers_factory import HandlersFactory
+from ..bot.state_machine import StateMachine
+from ..bot.storage.base_spreadsheet_storage import BaseSpreadsheetStorage
 
 
 class ProctoringBot(Bot):
+    """
+    Proctoring bot class implementation with run method.
+    """
+
     _logger = LogInstaller.get_default_logger(__name__, LogInstaller.INFO)
 
     def __init__(self, token, factory: HandlersFactory, storage: BaseSpreadsheetStorage):
