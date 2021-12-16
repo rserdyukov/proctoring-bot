103049bac0e76dc7f55b105d217bd8d98d41b5ed
NikitaZotov <nikita.zotov.belarus@gmail.com>
2021-12-17 00:30:42 +0300

    docs(handlers_chain): add docs strings about implementation

--- sources/bot/modules/handlers_chain.py
+++ sources/bot/modules/handlers_chain.py
@@ -1,5 +1,12 @@
+"""
+Bot handlers chain interface module.
+"""
 from abc import ABCMeta
 
 
 class HandlersChain:
+    """
+    Bot handlers chain interface.
+    """
+
     __metaclass__ = ABCMeta
