e884de9d3a55d68d0f98aec1e70234b62637f1ec
NikitaZotov <nikita.zotov.belarus@gmail.com>
2021-12-17 00:35:03 +0300

    docs(tools): add docs strings about implementation

--- sources/tools/config/base_config.py
+++ sources/tools/config/base_config.py
@@ -1,9 +1,48 @@
+"""
+Config interface module.
+"""
+from abc import ABCMeta, abstractmethod
+
+
 class BaseConfig:
+    """
+    Config interface.
+    """
+
+    __metaclass__ = ABCMeta
+
     def get_bot_option(self, option: str) -> str:
+        """
+        Gets bot options value.
+
+        :param option: Config option
+        :type option: :obj:`str`
+
+        :return: Returns bot option value.
+        :rtype: :obj:`str`
+        """
         raise NotImplementedError
 
     def get_chat_option(self, option: str) -> str:
+        """
+        Gets chat options value.
+
+        :param option: Config option
+        :type option: :obj:`str`
+
+        :return: Returns chat option value.
+        :rtype: :obj:`str`
+        """
         raise NotImplementedError
 
     def get_spreadsheet_option(self, option: str) -> str:
+        """
+        Gets spreadsheet options value.
+
+        :param option: Config option
+        :type option: :obj:`str`
+
+        :return: Returns spreadsheet option value.
+        :rtype: :obj:`str`
+        """
         raise NotImplementedError
