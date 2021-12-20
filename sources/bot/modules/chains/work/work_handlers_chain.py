37c634d4e1113f7cd080442053943f8386680eec
NikitaZotov <nikita.zotov.belarus@gmail.com>
2021-12-21 02:33:42 +0300

    refactor(work_chain): rename class

--- sources/bot/modules/chains/work/work_handlers_chain.py
+++ sources/bot/modules/chains/work/work_handlers_chain.py
@@ -11,27 +11,27 @@ from ...handlers_chain import HandlersChain
 from ...handlers_registrar import HandlersRegistrar as Registrar
 
 
-class Work(StatesGroup):
+class WorkStates(StatesGroup):
     waiting_for_link = State()
 
 
-class WorksHandlersChain(HandlersChain):
+class WorkHandlersChain(HandlersChain):
     _logger = LogInstaller.get_default_logger(__name__, LogInstaller.DEBUG)
 
     @staticmethod
     @Registrar.message_handler(commands=["lab"], state="*")
     async def lab_start_handler(message: types.Message):
-        WorksHandlersChain._logger.debug(f"Start lab conversation state")
-        await message.answer("Отправьте ссылку на лабораторную работу")
+        WorkHandlersChain._logger.debug(f"Start lab conversation state")
+        await message.answer("Отправьте ссылку на лабораторную работу.")
         await Work.waiting_for_link.set()
 
     @staticmethod
-    @Registrar.message_handler(state=Work.waiting_for_link)
+    @Registrar.message_handler(state=WorkStates.waiting_for_link)
     async def lab_link_send_handler(message: types.Message, state: FSMContext):
         if validators.url(str(message.text)):
             await state.update_data(works=message.text)
-            await message.answer("Ссылка успешно добавлена")
-            WorksHandlersChain._logger.debug(f"Finite lab conversation state")
+            await message.answer("Ссылка успешно добавлена.")
+            WorkHandlersChain._logger.debug(f"Finite lab conversation state")
             await state.finish()
         else:
-            await message.answer("Пожалуйста отправьте действительную ссылку")
+            await message.answer("Пожалуйста, отправьте действительную ссылку.")
