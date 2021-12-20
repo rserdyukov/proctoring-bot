f4ce2dee095d45a9603f440a3dd6008c862273c0
NikitaZotov <nikita.zotov.belarus@gmail.com>
2021-12-21 01:38:25 +0300

    fix(storage): fix interface polymorphism proccessing

--- sources/bot/storage/base_spreadsheet_storage.py
+++ sources/bot/storage/base_spreadsheet_storage.py
@@ -6,8 +6,8 @@ from abc import abstractmethod, ABCMeta
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from .spreadsheet.auth.base_auth_spreadsheet_handler import BaseAuthSpreadsheetHandler
-from .spreadsheet.tests.tests_spreadsheet_handler import TestsSpreadsheetHandler
-from .spreadsheet.works.works_spreadsheet_handler import WorksSpreadsheetHandler
+from .spreadsheet.tests.base_tests_spreadsheet_handler import BaseTestsSpreadsheetHandler
+from .spreadsheet.works.base_works_spreadsheet_handler import BaseWorksSpreadsheetHandler
 
 
 class BaseSpreadsheetStorage(MemoryStorage):
@@ -28,21 +28,21 @@ class BaseSpreadsheetStorage(MemoryStorage):
         raise NotImplementedError
 
     @abstractmethod
-    def visit_works_handler(self, works_handler: WorksSpreadsheetHandler):
+    def visit_works_handler(self, works_handler: BaseWorksSpreadsheetHandler):
         """
         Allows accessing to student works sending spreadsheet handler.
 
         :param works_handler: Student works sending spreadsheet handler instance
-        :type works_handler: :obj:`WorksSpreadsheetHandler`
+        :type works_handler: :obj:`BaseWorksSpreadsheetHandler`
         """
         raise NotImplementedError
 
     @abstractmethod
-    def visit_tests_handler(self, tests_handler: TestsSpreadsheetHandler):
+    def visit_tests_handler(self, tests_handler: BaseTestsSpreadsheetHandler):
         """
         Allows accessing to student tests managing spreadsheet handler.
 
         :param tests_handler: Student tests managing spreadsheet handler instance
-        :type tests_handler: :obj:`TestsSpreadsheetHandler`
+        :type tests_handler: :obj:`BaseTestsSpreadsheetHandler`
         """
         raise NotImplementedError
