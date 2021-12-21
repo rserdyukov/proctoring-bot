ed7f0eb4074841e2df6ad0153e48c81cb6ababa9
Vander <vander706@gmail.com>
2021-12-21 13:39:18 +0300

    feat(spreadsheet_handler): added spreadsheet handlers for tests module

--- sources/bot/storage/spreadsheet/tests/base_tests_spreadsheet_handler.py
+++ sources/bot/storage/spreadsheet/tests/base_tests_spreadsheet_handler.py
@@ -1,8 +1,22 @@
-from abc import ABCMeta
+from abc import abstractmethod, ABCMeta
 
-from bot.storage.spreadsheet.base_spreadsheet_handler import BaseSpreadsheetHandler
+from sources.bot.exceptions import SpreadsheetHandlerException
+from sources.bot.storage.spreadsheet.base_spreadsheet_handler import BaseSpreadsheetHandler
 
 
 class BaseTestsSpreadsheetHandler(BaseSpreadsheetHandler):
     __metaclass__ = ABCMeta
-    # todo: Formalise interface
+
+    def create_spreadsheet(self, spreadsheet_title: str = None, row_count: int = None, column_count: int = None):
+        pass
+
+    def accept_storage(self, storage):
+        storage.visit_works_handler(self)
+
+    @abstractmethod
+    def load_test_by_link(self, url: str) -> list:
+        raise SpreadsheetHandlerException("Not implemented method")
+
+    @abstractmethod
+    def add_result_to_worksheet(self, test_name, user_data, result_list) -> None:
+        raise SpreadsheetHandlerException("Not implemented method")
