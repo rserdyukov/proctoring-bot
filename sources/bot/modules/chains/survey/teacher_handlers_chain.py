ff37785b4c218e37ee4e12589bd1a8e5b792f7f7
Vander <vander706@gmail.com>
2021-12-21 13:44:07 +0300

    fix(teacher_handlers): changed handlers to work with links

--- sources/bot/modules/chains/survey/teacher_handlers_chain.py
+++ sources/bot/modules/chains/survey/teacher_handlers_chain.py
@@ -1,28 +1,40 @@
 from typing import List
 
+from urlvalidator import URLValidator, ValidationError
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
 from bot.loggers import LogInstaller
-from bot.modules.chains.survey.menu_handlers_chain import GeneralSurveyStates
 from bot.modules.handlers_chain import HandlersChain
 from bot.modules.handlers_registrar import HandlersRegistrar as Registrar
+from bot.storage.spreadsheet.tests.tests_spreadsheet_handler import TestsSpreadsheetHandler
 from bot.modules.keyboard.keyboard import KeyboardBuilder
 
 
 class SurveyTeacherStates(StatesGroup):
+    waiting_for_link = State()
     starting_survey = State()
 
 
 class SurveyTeacherKeyboardBuilder:
     @staticmethod
-    def get_start_survey_keyboard(self):
-        return KeyboardBuilder(
+    def get_cancel_survey_keyboard():
+        return KeyboardBuilder.get_inline_keyboard_markup(
+            [
+                {
+                    "Вернуться": "cancel_survey",
+                }
+            ]
+        )
+
+    @staticmethod
+    def get_start_survey_keyboard():
+        return KeyboardBuilder.get_inline_keyboard_markup(
             [
                 {
                     "Отправить": "send_survey",
-                    "Вернуться": "cancel_survey"
+                    "Вернуться": "cancel_survey",
                 }
             ]
         )
@@ -32,20 +44,35 @@ class SurveyTeacherHandlersChain(HandlersChain):
     _logger = LogInstaller.get_default_logger(__name__, LogInstaller.DEBUG)
 
     @staticmethod
-    @Registrar.callback_handler(text="start_survey", state=GeneralSurveyStates.teacher_survey)
+    @Registrar.message_handler(commands=["survey"])
+    async def survey_link_get_handler(message: types.Message):
+        await SurveyTeacherStates.waiting_for_link.set()
+        await message.answer("Отправьте ссылку на таблицу с тестом",
+                             reply_markup=SurveyTeacherKeyboardBuilder.get_cancel_survey_keyboard())
+
+    @staticmethod
+    @Registrar.message_handler(state=SurveyTeacherStates.waiting_for_link)
+    async def link_message_handler(message: types.Message):
+        validate = URLValidator()
+        try:
+            validate(message.text)
+            await SurveyTeacherStates.starting_survey.set()
+            await Registrar.bot.send_message(message.chat.id, text="Здесь будет тест")
+            await message.answer("Отправить опрос студентам?",
+                                 reply_markup=SurveyTeacherKeyboardBuilder.get_start_survey_keyboard())
+        except ValidationError as exception:
+            await message.answer("Неправильная ссылка, попробуйте еще раз",
+                                 reply_markup=SurveyTeacherKeyboardBuilder.get_cancel_survey_keyboard())
+
+    @staticmethod
+    @Registrar.callback_query_handler(text="start_survey", state=SurveyTeacherStates.starting_survey)
     async def start_survey_handler(query: types.CallbackQuery, state: FSMContext):
-        await SurveyTeacherStates.starting_survey.set()
-        await Registrar.bot.send_message(query.message.chat.id, text=SurveyTeacherHandlersChain.get_survey())
-        await query.message.edit_text("Отправить опрос студентам?")
+        await Registrar.bot.send_message(query.message.chat.id, text="Здесь будет тест")
+        await query.message.edit_text("Опрос отправлен n студентам",
+                                      reply_markup=SurveyTeacherKeyboardBuilder.get_cancel_survey_keyboard())
 
     @staticmethod
-    @Registrar.callback_handler(text="send_survey", state=SurveyTeacherStates.starting_survey)
-    async def send_survey_handler(query: types.CallbackQuery, state: FSMContext):
-        await query.message.edit_text("Опрос отправлен")
-        await state.reset_state()
-
-    @staticmethod
-    @Registrar.callback_handler(text="cancel_survey", state=SurveyTeacherStates.starting_survey)
+    @Registrar.callback_query_handler(text="cancel_survey", state="*")
     async def cancel_survey_handler(query: types.CallbackQuery, state: FSMContext):
         await query.message.edit_text("Отправка опроса отменена")
         await state.reset_state()
