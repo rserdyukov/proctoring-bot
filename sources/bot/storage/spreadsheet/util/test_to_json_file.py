5e45cf828b9a216c484c25c9e36cc49b60e0c3bc
Yauheni Krivoshei <YKrivoshei@iba.by>
2022-01-02 03:34:23 +0300

    feat(json_util): added get test from file method

--- sources/bot/storage/spreadsheet/util/test_to_json_file.py
+++ sources/bot/storage/spreadsheet/util/test_to_json_file.py
@@ -13,3 +13,8 @@ class JsonTestFileUtil:
             os.makedirs(path, exist_ok=True)
             with open(f'surveys/{test_name}.json', 'w', encoding='utf-8') as f:
                 json.dump(test, f, ensure_ascii=False, indent=4)
+
+    @staticmethod
+    def get_test_from_file(survey_sheet_name: str):
+        with open(f'surveys/{survey_sheet_name}.json', encoding='utf-8') as json_file:
+            return json.load(json_file)
